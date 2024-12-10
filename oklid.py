ef5520640d4e295086e8fac37646118373303861
tmcnmert <tmcnmert@gmail.com>
2024-12-10 17:28:16 +0300

    oklid.py

--- oklid.py
+++ oklid.py
@@ -1,5 +1,3 @@
-Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> import math
 ... points = [(1, 2), (4, 6), (7, 8), (2, 1)]
 ... 
